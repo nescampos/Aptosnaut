9ac4f5112429cc92f8a83b2cb753613dac93774f
Néstor Nicolás Campos Rojas <nescampos_18@hotmail.com>
2025-02-27 14:36:43 -0300

    Fix mainnet

--- Aptosnaut/Controllers/BaseController.cs
+++ Aptosnaut/Controllers/BaseController.cs
@@ -5,7 +5,7 @@ namespace Aptosnaut.Controllers
 {
     public class BaseController : Controller
     {
-        protected AptosClient client = new AptosClient(Networks.Testnet);
+        protected AptosClient client = new AptosClient(Networks.Mainnet);
         protected static Dictionary<string, EphemeralKeyPair> ekpDict = new Dictionary<string, EphemeralKeyPair>();
         protected static Dictionary<string, KeylessAccount> keylessAccounts = new Dictionary<string, KeylessAccount>();
     }
